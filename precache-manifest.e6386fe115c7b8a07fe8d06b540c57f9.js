2c7d65b5e374bf4ae974f2c6187df9f856d8c334
Verma <v.nishad@slnsoftwares.com>
2020-11-22 10:19:01 +0530

    Updates

---
+++ precache-manifest.e6386fe115c7b8a07fe8d06b540c57f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1343569234714c146df3b5f0042bdca",
+    "url": "/covid_trackergit/index.html"
+  },
+  {
+    "revision": "5db824a4dcc9d17f3c3c",
+    "url": "/covid_trackergit/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "80efa0a757458b876955",
+    "url": "/covid_trackergit/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5db824a4dcc9d17f3c3c",
+    "url": "/covid_trackergit/static/js/2.140fb08f.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid_trackergit/static/js/2.140fb08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80efa0a757458b876955",
+    "url": "/covid_trackergit/static/js/main.2c6032ca.chunk.js"
+  },
+  {
+    "revision": "b3221e1b4c3c57532c8f",
+    "url": "/covid_trackergit/static/js/runtime-main.2c382c09.js"
+  }
+]);
